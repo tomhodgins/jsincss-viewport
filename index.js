5620191c0a30b1232ca22509069cc6fa8bb7725a
Tommy Hodgins <tomhodgins@gmail.com>
2018-12-24 15:52:01 -0500

    Refactor to be cleaner/faster

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 module.exports = (selector, option, stylesheet) => {
-
+  const attr = (selector + option).replace(/\W/g, '')
   const features = {
-    partly: tag => {
-      return tag.offsetTop - innerHeight < scrollY
-        && tag.offsetTop - innerHeight + tag.offsetHeight < scrollY + tag.offsetHeight
-        && scrollY < tag.offsetTop + tag.offsetHeight
-    },
-    fully: tag => {
-      return tag.offsetTop - innerHeight < scrollY
-        && tag.offsetTop - innerHeight + tag.offsetHeight < scrollY
-        && scrollY < tag.offsetTop
-    }
+    partly: tag =>
+      tag.offsetTop - innerHeight < scrollY
+      && tag.offsetTop - innerHeight + tag.offsetHeight < scrollY + tag.offsetHeight
+      && scrollY < tag.offsetTop + tag.offsetHeight,
+    fully: tag =>
+      tag.offsetTop - innerHeight < scrollY
+      && tag.offsetTop - innerHeight + tag.offsetHeight < scrollY
+      && scrollY < tag.offsetTop
   }
-
-  return Array.from(document.querySelectorAll(selector))
-
-    .reduce((styles, tag, count) => {
-
-      const attr = (selector + option).replace(/\W/g, '')
-
+  const result = Array.from(document.querySelectorAll(selector))
+    .reduce((output, tag, count) => {
       if (features[option](tag)) {
-
-        tag.setAttribute(`data-viewport-${attr}`, count)
-        styles += stylesheet.replace(
-          /:self|\$this|\[--self\]/g,
-          `[data-viewport-${attr}="${count}"]`
+        output.add.push({tag: tag, count: count})
+        output.styles.push(
+          stylesheet.replace(
+            /:self|\$this|\[--self\]/g,
+            `[data-viewport-${attr}="${count}"]`
+          )
         )
-  
       } else {
-
-        tag.setAttribute(`data-viewport-${attr}`, '')
-
+        output.remove.push(tag)
       }
-
-      return styles
-
-    }, '')
-
+      return output
+    }, {add: [], remove: [], styles: []})
+  result.add.forEach(tag => tag.tag.setAttribute(`data-viewport-${attr}`, tag.count))
+  result.remove.forEach(tag => tag.setAttribute(`data-viewport-${attr}`, ''))
+  return result.styles.join('\n')
 }
